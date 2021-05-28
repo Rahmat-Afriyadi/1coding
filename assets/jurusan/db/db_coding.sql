31843a575b38228b6a54ccd49106fa304af99972
satrio nugroho <sn567530@gmail.com>
2021-05-28 12:16:34 +0700

    lalala

---
+++ assets/jurusan/db/db_coding.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 27, 2021 at 09:38 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_coding`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kepalakejuruan`
+--
+
+CREATE TABLE `kepalakejuruan` (
+  `nama_kepalakejuruan` varchar(35) NOT NULL,
+  `nama_jurusan` varchar(35) NOT NULL,
+  `gambar` varchar(35) NOT NULL,
+  `jumlah_guru` int(5) NOT NULL,
+  `jumlah_siswa` int(5) NOT NULL,
+  `jumlah_kelas` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
